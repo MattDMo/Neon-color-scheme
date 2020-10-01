8f1da54de757e0249d8e9ba4f4c3e21b3e5049f4
MattDMo <mattdmo@mattdmo.com>
2020-10-01 15:03:53 -0400

    commented everything out

--- neon_color_scheme.py
+++ neon_color_scheme.py
@@ -1,17 +1,17 @@
-import sublime
+# import sublime
 
-from package_control import events
+# from package_control import events
 
 
-package_name = "Neon Color Scheme"
+# package_name = "Neon Color Scheme"
 
-def plugin_loaded():
-    if events.install(package_name):
-        print("%s successfully installed!" % events.install(package_name))
-    elif events.post_upgrade(package_name):
-        settings = sublime.load_settings("Preferences.sublime-settings")
-        color_scheme = settings.get("color_scheme")
-        if color_scheme == "Packages/Neon Color Scheme/Neon.tmTheme":
-            settings.set("color_scheme", "Packages/Neon Color Scheme/Neon.sublime-color-scheme")
-            sublime.save_settings("Preferences.sublime-settings")
-            print("Successfully changed color_scheme!")
+# def plugin_loaded():
+#     if events.install(package_name):
+#         print("%s successfully installed!" % events.install(package_name))
+#     elif events.post_upgrade(package_name):
+#         settings = sublime.load_settings("Preferences.sublime-settings")
+#         color_scheme = settings.get("color_scheme")
+#         if color_scheme == "Packages/Neon Color Scheme/Neon.tmTheme":
+#             settings.set("color_scheme", "Packages/Neon Color Scheme/Neon.sublime-color-scheme")
+#             sublime.save_settings("Preferences.sublime-settings")
+#             print("Successfully changed color_scheme!")
